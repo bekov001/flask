5b9204de6a385b9540ae3d6a870e73344b5e0357
Anuar <ava200ava@gmail.com>
2022-02-20 16:23:02 +0600

    greate

--- main.py
+++ main.py
@@ -15,22 +15,13 @@ def train(prof):
     return render_template("index.html", text=choice(("Научные симуляторы", "Инженерные тренажеры")), prof=prof)
 
 
-@app.route('/answer')
-@app.route('/auto_answer')
-def auto_answer():
-    profil = {}
-    profil['title'] = 'Анкета'
-    profil['surname'] = 'Watny'
-    profil['name'] = 'Mark'
-    profil['education'] = 'выше среднего'
-    profil['profession'] = 'штурман марсохода'
-    profil['sex'] = 'male'
-    profil['motivation'] = 'Всегда мечтал застрять на Марсе!'
-    profil['ready'] = 'True'
-    return render_template('answer.html', **profil)
-
+@app.route('/distribution')
+def distribution():
+    return render_template('list.html', astronauts=astronauts)
 
 
 if __name__ == '__main__':
+    astronauts = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни',
+                  'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
     app.debug = True
     app.run(port=8080, host='127.0.0.1')
