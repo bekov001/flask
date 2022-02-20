7e5acbb31468bcae0b70cd4f6e601b80dfe60869
Anuar <ava200ava@gmail.com>
2022-02-20 15:53:54 +0600

    sec

--- main.py
+++ main.py
@@ -15,5 +15,18 @@ def train(prof):
     return render_template("index.html", text=choice(("Научные симуляторы", "Инженерные тренажеры")), prof=prof)
 
 
+@app.route('/list_prof/<type_lst>')
+def index(type_lst):
+    return render_template('list.html', url=type_lst, professions=profs)
+
+
+profs = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+
+
 if __name__ == '__main__':
+    app.debug = True
     app.run(port=8080, host='127.0.0.1')
