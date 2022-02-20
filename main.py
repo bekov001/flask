47d2c87a620ea4ea47b4d89a320d13c4e104ebc2
Anuar <ava200ava@gmail.com>
2022-02-20 15:41:53 +0600

    sec

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +8,11 @@ app = Flask(__name__)
 @app.route("/index/<name>")
 def main(name):
     return render_template('base.html', name=name)
+
+
+@app.route("/training/<prof>")
+def train(prof):
+    return render_template("index.html", text=choice(("Научные симуляторы", "Инженерные тренажеры")), prof=prof)
 
 
 if __name__ == '__main__':
